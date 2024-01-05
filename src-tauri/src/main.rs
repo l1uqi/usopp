c6566ebe54dbc797486a90f0b503a8dfbc54b0aa
liuqi <70128222+l1uqi@users.noreply.github.com>
2024-01-05 16:17:00 +0800

    feat: 新增系统托盘菜单

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,15 +2,23 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod command;
-
 mod init;
+mod tray;
+mod event;
 
 fn main() {
     init::init_app();
     tauri::Builder::default()
+        .setup(|app| {
+            event::resiger_event(&app);
+            Ok(())
+        })
+        .system_tray(tauri::SystemTray::new().with_menu(tray::tray_menu()))
+        .on_system_tray_event(tray::tray_event)
         .invoke_handler(tauri::generate_handler![
             command::search,
             command::open,
+            command::window_change
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,52 @@
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayEvent, Manager, SystemTrayMenuItem, AppHandle, api};
+
+
+pub fn tray_menu() -> SystemTrayMenu {
+  let open = CustomMenuItem::new("open_window".to_string(), "打开面板");
+  let hide = CustomMenuItem::new("hide".to_string(), "隐藏应用");
+  let restart = CustomMenuItem::new("restart_app".to_string(), "重启应用");
+  let update = CustomMenuItem::new("restart_app".to_string(), "获取更新");
+  let quit= CustomMenuItem::new("quit".to_string(), "退出").accelerator("CmdOrControl+Q");
+
+  SystemTrayMenu::new()
+  .add_item(open)
+  .add_item(hide)
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(restart)
+  .add_item(update)
+  .add_native_item(SystemTrayMenuItem::Separator)
+  .add_item(quit)
+}
+
+pub fn tray_event(app_handle: &AppHandle, event: SystemTrayEvent) {
+  match event {
+    SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        "open_window" => {
+          let window = app_handle.get_window("main").unwrap();
+          window.unminimize().unwrap();
+          window.show().unwrap();
+        },
+        "hide" => {
+          let window = app_handle.get_window("main").unwrap();
+          window.hide().unwrap();
+        }
+        "restart_app" => {
+          api::process::restart(&app_handle.env());
+        }
+        "update" => {
+
+        }
+        "quit" => {
+          std::process::exit(0);
+        }
+        _ => {}
+    },
+    #[cfg(target_os = "windows")]
+    SystemTrayEvent::LeftClick { .. } => {
+      let window = app_handle.get_window("main").unwrap();
+      window.unminimize().unwrap();
+      window.show().unwrap();
+    }
+    _ => {}
+}
+}
